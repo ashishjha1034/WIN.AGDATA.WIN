1ed7755f91e61c30e39109fc5f5f11622b96b0c2
Ashish Jha <ashishjha1034@gmail.com>
2026-02-05 16:20:21 +0530

    deleted few files to look clean

--- HashPassword.cs
+++
@@ -1,6 +0,0 @@
-using BCrypt.Net;
-
-var password = "Admin@123456";
-var hash = BCrypt.HashPassword(password);
-Console.WriteLine($"Password: {password}");
-Console.WriteLine($"BCrypt Hash: {hash}");
--- fix_passwords.sql
+++
@@ -1,8 +0,0 @@
--- Fix password hashes with correct BCrypt format
--- Password: Password@123
-
-UPDATE Users 
-SET PasswordHash = '$2a$11$6kybXtmlYxHZsQS0itXCZOcGImL2ydA0eaqY4.zvfmDZlmHXEIdAO'
-WHERE Email LIKE '%@agdata.com';
-
-SELECT TOP 1 Email, LEN(PasswordHash) as HashLen, PasswordHash FROM Users;
